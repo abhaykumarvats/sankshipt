8af2af8b850d5fe2cfb567ac226ae7953d00f543
Abhay Kumar <abhaykv04@gmail.com>
2018-04-25 22:14:39 +0530

    Add starter code

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+// Require modules
 const app = require('express')();
+const assert = require('assert');
 const validUrl = require('valid-url');
+const URL = require('./schemas/URL');
+
+// Assign port number
+let PORT = process.env.PORT || 3000;
 
 var PORT = process.env.PORT || 3000;
 
+// Listen for requests
 app.listen(PORT, () => {
     console.log('(sankshipt): Listening on port', PORT);
 });
