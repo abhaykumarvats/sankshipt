d01542610a6967599a9c8ede94c88596417c1c81
Abhay Kumar <abhaykv04@gmail.com>
2018-04-25 17:07:08 +0530

    Add starter code

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const app = require('express')();
 const validUrl = require('valid-url');
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('(sankshipt): Listening on port', PORT);
+});
